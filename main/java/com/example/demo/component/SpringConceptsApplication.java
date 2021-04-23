1c738ea31f98dbf1a11225e1a8664f5cb912dbc6
viresh241196 <vireshrawool007@gmail.com>
2021-04-23 16:48:35 +0530

    added method to get the log

--- main/java/com/example/demo/component/SpringConceptsApplication.java
+++ main/java/com/example/demo/component/SpringConceptsApplication.java
@@ -1,16 +1,20 @@
 package com.example.demo.component;
 
 import org.springframework.context.ApplicationContext;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 
 	public static void main(String[] args) {
+		logger.debug("Welcome to spring concept Demo");
 		System.out.println("welcome to Spring concept Demo");
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class,args);
 		DemoBean demoBean = context.getBean(DemoBean.class);
-		System.out.println("DemoBean ="+demoBean.toString());
+		logger.debug("DemoBean ="+demoBean.toString());
 	}
 }
